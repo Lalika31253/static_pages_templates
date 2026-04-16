e1f315acbdeefcbead8d2040d4ef33e1b84c18c5
Alina <Lalika31253@gmail.com>
2026-04-15 22:05:35 -0600

    Add news template

--- stat_pgs_tmpl/views.py
+++ stat_pgs_tmpl/views.py
@@ -2,19 +2,33 @@ from django.shortcuts import render
 
 def home(request):
     ctx = {
-        'name': "ComIT",
-        'students': 10,
-        'names': ["Bob", "Sarah", "Don", "Inna", "Alex", "Tim", "Greg", "Anna", "Ryan", "Sam"],
-        'is_active': True
+        "theme": "cupcake",
+        "name": "ComIT",
+        "students": 10,
+        "names": ["Bob", "Sarah", "Don", "Inna", "Alex", "Tim", "Greg", "Anna", "Ryan", "Sam"],
+        "is_active": True
     }
     return render(request, 'stat_pgs_tmpl/home.html', ctx)
 
 
 def contact(request):
   ctx = {
-     'medias': ["Facebook", "LinkedIn", "Instagram", "Telegram"]
+     "theme": "retro",
+     "medias": ["Facebook", "LinkedIn", "Instagram", "Telegram"]
   }
   return render(request, 'stat_pgs_tmpl/contact.html', ctx)
 
+
 def about(request):
-    return render(request, 'stat_pgs_tmpl/about.html')
+    ctx = {
+        "theme": "valentine"
+    }
+    return render(request, 'stat_pgs_tmpl/about.html', ctx)
+
+
+def news(request):
+    ctx = {
+        "theme": "caramellatte",
+        "news": ["February", "March", "April"]
+    }
+    return render(request, 'stat_pgs_tmpl/news.html', ctx)
